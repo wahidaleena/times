2ca67bc7acdcfff594010d4037c49288d18d687d
wahidaleena <34679809+wahidaleena@users.noreply.github.com>
2019-06-21 12:37:01 +0530

    Update perform_times.cbl

--- perform_times.cbl
+++ perform_times.cbl
@@ -6,10 +6,10 @@ IDENTIFICATION DIVISION.
        01 WS-SUB   PIC 9  VALUE ZEROS.
        PROCEDURE DIVISION.
        10000-MAIN-PARA.
-             MOVE 5 TO WS-SUB.
-             PERFORM 20000-FIRST-PARA 5 TIMES
-             DISPLAY 'PARA NAME IS  10000-FIRST-PARA'
-             STOP RUN.
+              MOVE 5 TO WS-SUB.
+              PERFORM 20000-FIRST-PARA 5 TIMES
+              DISPLAY 'PARA NAME IS  10000-FIRST-PARA'
+              STOP RUN.
        20000-FIRST-PARA.
-             DISPLAY 'VALUE OF WS-SUB IS = ' WS-SUB.
-             ADD +1 TO WS-SUB.
+              DISPLAY 'VALUE OF WS-SUB IS = ' WS-SUB.
+              ADD +1 TO WS-SUB.
